1f4b59e18bcd595cac517c7c8d6463d1968cb1d8
Timmi00 <TIm200988>
2022-09-22 17:06:03 +0300

    add new tasks

--- lesson_1/req.py
+++ lesson_1/req.py
@@ -1,20 +1,22 @@
 from requests import Session
 
+
 def get_response():
     with Session() as session:
         session.headers.update({'Accept-Language': 'ru'})
         response = session.get(
             url='https://catalog.onliner.by/sdapi/catalog.api/search/player',
             params={
-                'player_type[0]':'hifiplayer',
-                'player_type[operation]':'union',
-                'group' : 1
+                'player_type[0]': 'hifiplayer',
+                'player_type[operation]': 'union',
+                'group': 1
             }
         )
         print(response.status_code)
         print(response.headers)
         print(response.text)
         print(response.json())
+
 
 for i in range(100):
     get_response()
